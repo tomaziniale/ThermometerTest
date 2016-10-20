938e51d5b42a6ead809f78398e1b2a6f63a4a8c7
tomaziniale <tomazini.alexandre@gmail.com>
2016-10-20 00:35:39 -0700

    Add project files.

---
+++ ThermometerTest/Program.cs
@@ -0,0 +1,123 @@
+﻿/* 
+ * Name: Alexandre
+ * Last Name: Da silva
+ */
+
+using System;
+using System.Linq;
+
+namespace ThermometerTest
+{
+    /// <summary>
+    /// A thermometer that read temperature values from console input and shows alerts if the temperature
+    /// reaches determined threshold ( must be in the fluctuation range only)
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            // This is the defaults values for thresholds
+            var thermometer = new ThermometerModel
+            {
+                TemperatureType = "F",
+                Boiling = 212.0,
+                Freezing = 32.0,
+                Fluctuation = 3.6
+            };
+
+            // The default values can be modified in the lines above or during the execution
+            // by writing the name of the variable and a new value
+            // Examples: Boiling 98 C
+            // Fluctuation: 0.5 C
+            // by changing the threshold values, all alerts are reseted
+
+            //flag if the alert was shown
+            bool alerted = false;
+            // flag the direction of the last input, false = freezing, true = boiling, null = middle
+            bool? lastDirection = null;
+
+            //input (console only)
+            Console.WriteLine("Type 'defaults' to see the initialized values");
+            Console.WriteLine("Press Ctrl+Break ou Ctrl+C to exit");
+
+            while (true)
+            {
+                var inputValues = Console.ReadLine();
+
+                Interpreter(thermometer, inputValues);
+
+                if (thermometer.TriggerAlert())
+                {
+                    if (!alerted && thermometer.Direction != lastDirection)
+                        ShowAlert();
+                    else
+                        alerted = false;
+                }
+
+                lastDirection = thermometer.Direction;
+            }
+        }
+
+        /// <summary>
+        /// Do something related to the alert
+        /// </summary>
+        static void ShowAlert()
+        {
+            Console.WriteLine("Alert");
+        }
+
+        /// <summary>
+        /// Set the defaults or a value for the temperature
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="input_string"></param>
+        static void Interpreter(ThermometerModel model, string input_string)
+        {
+            var inputList = input_string.Split(' ');
+            double degree = 0;
+
+            if (input_string.ToLower().Equals("defaults"))
+                ShowDefaults(model);
+
+            model.ValidEntry = false;
+            model.TemperatureType = inputList.Last().ToUpper();
+           
+            //If the temperature type was not indicated, exit the method
+            if (!model.ValidTypes.ContainsKey(model.TemperatureType)) return;
+
+            string[] keywords = { "Boiling", "Freezing", "Fluctuation" };
+
+            if (keywords.Contains(inputList.First()))
+            {
+                // save the last value before, in case of conversion errors
+                var lastValue = (double)model.GetType().GetProperty(inputList.First()).GetValue(model, null);
+
+                // write the new value
+                degree = Double.TryParse(inputList[1], out degree) ? degree : lastValue;
+                model.GetType().GetProperty(inputList.First()).SetValue(model, degree);
+
+                // reset alerts
+                model.Direction = null;
+                // show defaults
+                ShowDefaults(model);
+            }
+            else
+            {
+                model.ValidEntry = Double.TryParse(inputList.First(), out degree);
+                model.Temperature = degree;
+            }
+        }
+
+        /// <summary>
+        /// Show the defaults values in the screen
+        /// </summary>
+        /// <param name="model"></param>
+        static void ShowDefaults(ThermometerModel model)
+        {
+            Console.WriteLine("Boiling value is set to: {0}° F", model.Boiling);
+            Console.WriteLine("Freezing value is set to: {0}° F", model.Freezing);
+            Console.WriteLine("Fluctuation value is set to: {0}° F", model.Fluctuation);
+        }
+    }
+}
---
+++ ThermometerTest/ThermometerModel.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+
+namespace ThermometerTest
+{
+    /// <summary>
+    /// Thermometer representation
+    /// </summary>
+    public class ThermometerModel
+    {
+        /// <summary>
+        /// Used to check if the last input was in a valid temperature format
+        /// </summary>
+        public bool ValidEntry { get; set; }
+        public string TemperatureType { get; set; }
+        public Dictionary<string, double> ValidTypes = new Dictionary<string, double> { { "F", 0 }, { "C", 32 }, { "K", 0 } };
+
+        private double _temperature { get; set; }
+        public double Temperature
+        {
+            get
+            {
+                return this._temperature;
+            }
+            set { this._temperature = this.converter(value); }
+        }
+
+        public bool? Direction { get; set; }
+
+        private double _freezing { get; set; }
+        public double Freezing
+        {
+            get { return this._freezing; }
+            set
+            {
+                this._freezing = converter(value);
+            }
+        }
+
+        private double _fluctuation { get; set; }
+        public double Fluctuation
+        {
+            get { return this._fluctuation; }
+            set
+            {
+                this._fluctuation = converter(value, ValidTypes[this.TemperatureType]);
+            }
+        }
+
+        private double _boiling { get; set; }
+        public double Boiling
+        {
+            get { return this._boiling; }
+            set
+            {
+                this._boiling = converter(value);
+            }
+        }
+
+        /// <summary>
+        /// Convert celsius to farenheit
+        /// this program only store values in farenheit but can work with celsius as well
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        private double converter(double v, double coefficient = 0)
+        {
+            switch (this.TemperatureType)
+            {
+                // to Farenheit
+                case "C":
+                    return v * 1.8 + 32 - coefficient;
+                // to Kelvin
+                case "K":
+                    return v * 1.8 - 459.67;
+                default:
+                    return v;
+            }
+        }
+
+        /// <summary>
+        /// Indicate if the temperature reached the threshold limited by the fluctuation range
+        /// and sets the direction of the temperature: false = freezing limits, true = boiling limits, null = out of thresholds
+        /// </summary>
+        /// <returns></returns>
+        public bool TriggerAlert()
+        {
+            if (!ValidEntry) return false;
+
+            bool result = false;
+            this.Direction = null;
+
+            if ((this.Temperature >= this.Freezing - this.Fluctuation) &&
+                    (this.Temperature <= this.Freezing + this.Fluctuation))
+            {
+                this.Direction = false;
+                result = true;
+            }
+            else if ((this.Temperature >= this.Boiling - this.Fluctuation) &&
+                (this.Temperature <= this.Boiling + this.Fluctuation))
+            {
+                this.Direction = true;
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
